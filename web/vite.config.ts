9e2a0f789e3668ae4309b2c4cee33754c9f17d3d
EriikGabriel <erikgabriel.lins@hotmail.com>
2023-09-17 23:52:28 -0300

    feat: add web

---
+++ web/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
